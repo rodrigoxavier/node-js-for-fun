2ed5d4edd533cb5e9852b9428417d3f4dfd8ca5b
Rodrigo Xavier <rodrigo.henrique@take.net>
2022-02-13 10:54:43 -0300

    Using next and a logger function every called route

--- appExpress.js
+++ appExpress.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const { route } = require('./routes/users');
-
 const app = express();
+
+app.use(logger); //Everything after this will use the logger.
 
 const practicingStatusRouter = require('./routes/practicingStatusRouter');
 const usersRoter = require('./routes/users');
 
 app.use('/status', practicingStatusRouter);
 app.use('/users', usersRoter);
+
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+}
 
 app.listen(4000);
